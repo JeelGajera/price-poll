c9e53262e89d8c33d26b63835c4bdbdd240e70cc
Jeel Gajera <jeelgajera00@gmail.com>
2023-10-05 15:40:08 +0530

    init project setup

---
+++ components/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <header className='w-full'>
+            <nav className=''>
+
+            </nav>
+        </header>
+    ); 
+};
+
+export default Navbar; 
